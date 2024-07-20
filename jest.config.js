ace03e888bc0854b6a5f9a83429187f558f0da25
carine <105859@ecole-it.com>
2024-07-20 22:46:53 +0200

    config supplémentaire pour le test

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
     testEnvironment: 'jest-environment-jsdom',
     roots: ['<rootDir>/test'],
     setupFilesAfterEnv: ['./jest.setup.js'],
+    transform: {
+      '^.+\\.js$': 'babel-jest'
+    }
   };
   
